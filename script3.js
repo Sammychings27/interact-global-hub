9a2e977b68b4167bf6ae861151d29c2f0e8e2c46
Sammychings27 <sammychings27@gmail.com>
2026-03-16 06:12:42 +0200

    Fix element ID in counter update

--- script3.js
+++ script3.js
@@ -2,7 +2,7 @@ let target = 159;
 let current = 1;
 
 let counter = setInterval(() => {
-    document.getElementById("counter2").textContent = current + "+";
+    document.getElementById("count").textContent = current + "+";
 
     if (current >= target) {
         clearInterval(counter); // stop at 159+
